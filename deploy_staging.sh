435bf06ede3f1278b2344d30fed13b54482dae97
Yasutaka Nariki <yasutaka-nariki@aquaring.co.jp>
2015-05-14 18:30:49 +0900

    Fix deploy script

--- deploy_staging.sh
+++ deploy_staging.sh
@@ -1,17 +1,31 @@
 #!/bin/bash
+# 
+
 set -ex
 
 
-ssh 52.68.225.208 "cd /var/www/www.fc-hikaku.net/rails; git reset --hard HEAD; git checkout staging; git pull; bundle install; bundle exec rake assets:clean RAILS_ENV=staging; bundle exec rake assets:precompile RAILS_ENV=staging; bundle exec rake db:migrate RAILS_ENV=staging; rake unicorn:stop; rake unicorn:start; exit 0"
+ssh 52.68.225.208 << EOF
 
+source ~/.bash_profile
 
-# cd /var/www/www.fc-hikaku.net/rails
-# git reset --hard HEAD
-# git checkout staging
-# git pull
-# bundle install
-# bundle exec rake assets:clean RAILS_ENV=staging
-# bundle exec rake assets:precompile RAILS_ENV=staging
-# bundle exec rake db:migrate RAILS_ENV=staging
-# rake unicorn:stop
-# rake unicorn:start
+cd /var/www/www.fc-hikaku.net/rails
+
+git reset --hard HEAD
+git checkout staging
+git pull
+
+bundle install
+
+bundle exec rake assets:clean RAILS_ENV=staging
+echo "rake assets:clean"
+bundle exec rake assets:precompile RAILS_ENV=staging
+echo "rake assets:precompile"
+bundle exec rake db:migrate RAILS_ENV=staging
+echo "rake db:migrate"
+
+rake unicorn:stop
+rake unicorn:start
+
+exit 0
+
+EOF
