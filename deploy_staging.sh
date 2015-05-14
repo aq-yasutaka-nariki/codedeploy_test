c83c838746a3eb50178a6d7d08bb50a5ff685f2e
Yasutaka Nariki <yasutaka-nariki@aquaring.co.jp>
2015-05-14 17:07:50 +0900

    fix deploy script

--- deploy_staging.sh
+++ deploy_staging.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 set -ex
 
-#ssh 52.68.225.208 'touch test.txt'
-#ssh 52.68.225.208 "cd /var/www/deploy_test; touch test.txt"
-ssh 52.68.225.208 "cd /var/www/www.fc-hikaku.net/rails; git reset --hard HEAD; git pull; bundle install; rake unicorn:stop; rake unicorn:start;"
+
+ssh 52.68.225.208 "cd /var/www/www.fc-hikaku.net/rails; git reset --hard HEAD; git checkout staging; git pull; bundle install; bundle exec rake assets:clean RAILS_ENV=staging; bundle exec rake assets:precompile RAILS_ENV=staging; bundle exec rake db:migrate RAILS_ENV=staging; rake unicorn:stop; rake unicorn:start; exit 0"
 
 
 # cd /var/www/www.fc-hikaku.net/rails
 # git reset --hard HEAD
+# git checkout staging
 # git pull
 # bundle install
+# bundle exec rake assets:clean RAILS_ENV=staging
+# bundle exec rake assets:precompile RAILS_ENV=staging
+# bundle exec rake db:migrate RAILS_ENV=staging
 # rake unicorn:stop
 # rake unicorn:start
-
-# bundle exec rake assets:clean RAILS_ENV=aq_staging
-# bundle exec rake assets:precompile RAILS_ENV=aq_staging
-# bundle exec rake db:migrate RAILS_ENV=aq_staging
-
